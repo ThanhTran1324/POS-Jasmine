77e1ed7d7317a45d4d55f853fc3c720a64d78d26
thanhtran1324 <thanhtran1324@gmail.com>
2020-10-24 10:31:17 -0400

    add new firebase key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,15 +7,25 @@ export const environment = {
 	backgroundApi: 'https://api.unsplash.com/photos/random/?client_id=b2698351b29cd4ed48f4205973ce312cdb11a61c71da5ae81a2c2630c2af3df1&collections=148982&orientation=landscape',
 	loginUrl: 'http://localhost:4200',
 	firebase: {
-		apiKey: 'AIzaSyBT1a45gappk9yNTzbqVCcaxjKHyyLftQ4',
-		authDomain: 'point-of-sale-5e574.firebaseapp.com',
-		databaseURL: 'https://point-of-sale-5e574.firebaseio.com',
-		projectId: 'point-of-sale-5e574',
-		storageBucket: 'point-of-sale-5e574.appspot.com',
-		messagingSenderId: '951585599142',
-		appId: '1:951585599142:web:095f365f27ec53f15bce67',
-		measurementId: 'G-9N5VFSGYNW'
+		apiKey: 'AIzaSyBrAwFPYkJ7EOuulVVsezmAr148zHUp5Qc',
+		authDomain: 'point-of-sale-63d3b.firebaseapp.com',
+		databaseURL: 'https://point-of-sale-63d3b.firebaseio.com',
+		projectId: 'point-of-sale-63d3b',
+		storageBucket: 'point-of-sale-63d3b.appspot.com',
+		messagingSenderId: '56735268539',
+		appId: '1:56735268539:web:e0980ee982c618a585b39e',
+		measurementId: 'G-BRFH340524'
 	}
+	// firebase: {
+	// 	apiKey: 'AIzaSyBT1a45gappk9yNTzbqVCcaxjKHyyLftQ4',
+	// 	authDomain: 'point-of-sale-5e574.firebaseapp.com',
+	// 	databaseURL: 'https://point-of-sale-5e574.firebaseio.com',
+	// 	projectId: 'point-of-sale-5e574',
+	// 	storageBucket: 'point-of-sale-5e574.appspot.com',
+	// 	messagingSenderId: '951585599142',
+	// 	appId: '1:951585599142:web:095f365f27ec53f15bce67',
+	// 	measurementId: 'G-9N5VFSGYNW'
+	// }
 };
 
 /*
